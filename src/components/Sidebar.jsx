c67f5d793804515e8723cc4dfcc5130fdc619f54
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-01-09 12:15:26 +0100

    edited the html file

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import chevron from '../assets/chevron logo.png'
 const Sidebar = () => {
   return (
     <div className="h-[1024px] w-[250px] py-4" style={{backgroundColor:"var(--Color-Base-Text-Color, #1C065A)"}}>
-     
-      
+    <div style={{display:'inline-flex', justifyContent:'center', alignItems:'center', gap:'8px'}}>
+     <img className='w=[43px] h-[43px] rounded-sm content-center' src={chevron} alt='logo'/>
+     <p>Chevron Group</p>
+      </div>
     </div>
   )
 }
