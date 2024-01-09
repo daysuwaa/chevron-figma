3b5057158cc9627f44288acb3567792a0f3a4643
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-01-09 12:04:05 +0100

    Added/created assets

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className="h-[1024px] w-[250px] py-4" style={{backgroundColor:"var(--Color-Base-Text-Color, #1C065A)"}}>
+     
+      
+    </div>
   )
 }
 
