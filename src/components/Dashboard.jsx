110c91ed6069a43c46bbd25da5642487c5e24c95
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-01-09 18:27:43 +0100

    new asset added

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import man from '../assets/image 1.png'
 
 const Dashboard = () => {
   return (
     <div>
       <div className='flex px-[18px] py-[32px] items-start'>
         <p>Dashboard</p>
+        <img src ={man} alt='man' className='justify-end'/>
 
       </div>
      
