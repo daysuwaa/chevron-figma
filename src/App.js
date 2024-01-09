88cf47e6ecacd01da0ab721e4540f0f8cecd9918
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-01-09 11:41:11 +0100

    Added the componenets to my App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React from 'react';
+import Dashboard from './components/Dashboard';
+import Payment from './components/Payment';
+import Schedules from './components/Schedules';
+import Beneficiaries from './components/Beneficiaries';
+import Approvals from './components/Approvals';
+import Users from './components/Users';
+import Analytics from './components/Analytics'
+import Settings from './components/Settings'
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router basename='/'>
+      <Routes>
+        <Route path="*" element={<Dashboard/>} />
+        <Route path="/chevron/payment" element={<Payment />} />
+        <Route path="/chevron/schedules" element={ <Schedules/>} />
+        <Route path="/chevron/beneficiaries" element={<Beneficiaries />} />
+        <Route path="/chevron/approvals" element={<Approvals />} />
+        <Route path="/chevron/users" element={<Users />} />
+        <Route path="/chevron/analytics" element={<Analytics />} />
+        <Route path="/chevron/settings" element={<Settings />} />
+       </Routes>
+    </Router>
   );
 }
 
