a29ae64fa36b3cd46d90169af9c1e1f6c676d8b7
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-01-09 12:30:08 +0100

    new assets added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,11 +3,19 @@ import chevron from '../assets/chevron logo.png'
 const Sidebar = () => {
   return (
     <div className="h-[1024px] w-[250px] py-4" style={{backgroundColor:"var(--Color-Base-Text-Color, #1C065A)"}}>
-    <div style={{display:'inline-flex', justifyContent:'center', alignItems:'center', gap:'8px'}}>
-     <img className='w=[43px] h-[43px] rounded-sm content-center' src={chevron} alt='logo'/>
-     <p>Chevron Group</p>
-      </div>
+
+        {/* the image and title */}
+    <div className="flex justify-center items-center space-x-2 py-6">
+  <img className="w-10 h-10 rounded-sm" src={chevron} alt="logo"/>
+  <p className="text-white font-semibold tracking-wider" style={{fontSize:'16px', lineHeight:'24px'}}>Chevron Group</p>
+  </div>
+  {/* end of image */}
+
+ 
+<div className="w-[250px] h-[1px] bg-gray-200"></div>
+
     </div>
+    // end of side bar
   )
 }
 
