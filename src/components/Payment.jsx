866f938d81cccc036f6fe20b588cb378ca9fc04d
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-01-13 20:36:55 +0100

    Payment

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -1,20 +1,9 @@
+import React from 'react'
 
-import React from 'react';
-
-const GridComponent = () => {
+const Payment = () => {
   return (
-    <div className="container ">
-      <div className="grid grid-cols-3 gap-4">
-        <div className="bg-gray-300 p-4 text-center">Box 1</div>
-        <div className="bg-gray-300 p-4 text-center">Box 2</div>
-        <div className="bg-gray-300 p-4 text-center">Box 3</div>
-      </div>
-      <div className="grid grid-cols-3 gap-4 mt-4">
-        <div className="bg-gray-300 p-4 text-center col-span-2">Box 4</div>
-        <div className="bg-gray-300 p-4 text-center">Box 5</div>
-      </div>
-    </div>
-  );
-};
+    <div>Payment</div>
+  )
+}
 
-export default GridComponent;
+export default Payment
