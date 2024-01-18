c9ed3d19aac3b53ed8646abf1fe4f10dea487da7
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-01-18 22:11:34 +0100

    Created a new file - Users

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import man from "../assets/image 1.png";
+import arrowdown from "../assets/FiChevronDown.svg";
 
 const Users = () => {
   return (
-    <div>Users</div>
-  )
-}
+    <div>
+      <div
+        className="flex px-[32px] py-[18px] justify-between h-[60px]"
+        style={{ background: "#fff" }}
+      >
+        <p className="font-semibold tracking-wide ">Users</p>
+        <div className="flex items-center">
+          <img src={man} alt="arrow" className="w-[24px] h-[24px] mr-3" />
+          <img src={arrowdown} alt="man" className="w-[14px] h-[14px]" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Users
+export default Users;
