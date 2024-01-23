1c5510f8194fb000e8918195aad507fc877dbdb0
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-01-23 14:01:13 +0100

    made the sidebar not move

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import React from 'react';
-import Dashboard from './components/Dashboard';
-import Payment from './components/Payment';
-import Schedules from './components/Schedules';
-import Beneficiaries from './components/Beneficiaries';
-import Approvals from './components/Approvals';
-import Users from './components/Users';
-import Analytics from './components/Analytics'
-import Settings from './components/Settings'
-import Sidebar from './components/Sidebar';
-import Error from './components/Error';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
+import Dashboard from "./components/Dashboard";
+import Payment from "./components/Payment";
+import Schedules from "./components/Schedules";
+import Beneficiaries from "./components/Beneficiaries";
+import Approvals from "./components/Approvals";
+import Users from "./components/Users";
+import Analytics from "./components/Analytics";
+import Settings from "./components/Settings";
+import Sidebar from "./components/Sidebar";
+import Error from "./components/Error";
+
 function App() {
   return (
-    <Router basename='/'>
+    <Router basename="/">
       {/* to make the side bar at the , yk ,side, lol */}
       <div className="app flex h-screen">
-        <Sidebar/>
-        <div style={{flex: 1}}className='max-h-full'>
+        <Sidebar />
+        <div className="content max-h-full overflow-y-auto" style={{ flex: 1 }}>
+          {/* Set a margin to the content area, adjust as needed */}
           <Routes>
-            <Route path="/" element={<Dashboard/>} />
+            <Route path="/" element={<Dashboard />} />
             <Route path="/payment" element={<Payment />} />
-            <Route path="/schedules" element={ <Schedules/>} />
+            <Route path="/schedules" element={<Schedules />} />
             <Route path="/beneficiaries" element={<Beneficiaries />} />
             <Route path="/approvals" element={<Approvals />} />
             <Route path="/users" element={<Users />} />
             <Route path="/analytics" element={<Analytics />} />
             <Route path="/settings" element={<Settings />} />
-            <Route path='/error' element={<Error/>} />
+            <Route path="/error" element={<Error />} />
           </Routes>
         </div>
       </div>
