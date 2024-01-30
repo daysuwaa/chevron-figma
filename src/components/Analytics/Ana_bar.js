5601affff91795a65b1e161e3e2081c673ccc112
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-01-30 11:50:47 +0100

    #2

--- src/components/Analytics/Ana_bar.js
+++ src/components/Analytics/Ana_bar.js
@@ -86,7 +86,7 @@ const data = [
 export default class Example extends PureComponent {
   render() {
     return (
-      <ResponsiveContainer width="100%" height="87%">
+      <ResponsiveContainer width="100%" height="83%">
         <BarChart
           width={500}
           height={300}
