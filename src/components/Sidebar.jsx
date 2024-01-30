532f48f6b02d41964d513ca6599c10ff0c08d54e
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-01-30 09:25:17 +0100

    side bar responsive #2

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -58,13 +58,10 @@ const Sidebar = () => {
   };
 
   return (
-    <div className="h-full w-[250px] py-4 bg-[#1C065A]">
+    <div className="h-full md:w-[250px] w-[70px] py-4 bg-[#1C065A]">
       <div className="flex justify-center items-center space-x-2 py-6">
         <img className="w-10 h-10 rounded-sm" src={chevron} alt="logo" />
-        <p
-          className="text-white font-semibold tracking-wider"
-          style={{ fontSize: "16px", lineHeight: "24px", fontWeight: "" }}
-        >
+        <p className="text-white font-semibold tracking-wider hidden md:block text-[16px] leading-[24px] ">
           Chevron Group
         </p>
       </div>
