b581532c0350c706dd91511c7ba48de5e9884133
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-02-06 18:20:04 +0100

    imported two new assets, user and left , then declared a function details with multiple parameters

--- src/components/Stuff/Details.jsx
+++ src/components/Stuff/Details.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-
-const Details = () => {
-  return <div>Details</div>;
+import Navbar from "./Navbar";
+import left from "src/assets/FiChevronLeft.svg";
+import use from "src/assets/user 1.svg";
+const Details = (
+  tab,
+  transaction_no,
+  address,
+  schedule_id,
+  date,
+  schedule_name,
+  payment_status,
+  amount_paid,
+  bank,
+  account_no
+) => {
+  return (
+    <div>
+      <Navbar title="Payment" />
+    </div>
+  );
 };
 
 export default Details;
