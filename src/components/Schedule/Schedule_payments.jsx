6ae94b4e4c5ab6126627988fbe63918b4cbf0284
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-02-07 16:17:48 +0100

    made schedule payment resposnsive

--- src/components/Schedule/Schedule_payments.jsx
+++ src/components/Schedule/Schedule_payments.jsx
@@ -42,7 +42,7 @@ const Schedule_payments = () => {
     return (
       <div>
         <Link to="/scheduletable">
-          <div className="flex mx-7 my-5 items-center">
+          <div className="flex mx-3 md:mx-7 my-5 items-center">
             <svg
               width="14"
               height="14"
@@ -70,19 +70,20 @@ const Schedule_payments = () => {
         </Link>
 
         {/* chevron salaries */}
-        <div className="grid grid-cols-3">
-          <div className="col-span-2 bg-white mx-7">
-            <div className="flex mx-7 pt-6">
+        {/* md7 lg screen */}
+        <div className="md:grid grid-cols-3">
+          <div className="hidden md:block col-span-2 bg-white mx-3 md:mx-7">
+            <div className="flex mx-3 md:mx-7 pt-6">
               <h5 className="text-[#1C065A] font-[500] text-[14px]">{grade}</h5>
               <div
                 className="flex ml-auto items-center "
                 onClick={copyToClipboard}
               >
-                <h5 className="font-light text-[14px] mx-2 mr- text-[#4A5568]">
+                <h5 className="font-light text-[14px] mr-2   text-[#4A5568]">
                   Schedule Id
                 </h5>
                 <svg
-                  className="mr-9 cursor-pointer"
+                  className="mr-5 cursor-pointer"
                   width="12"
                   height="12"
                   viewBox="0 0 12 12"
@@ -113,7 +114,9 @@ const Schedule_payments = () => {
               <p className="text-[14px] font-light text-[#4A5568]">
                 {no_of_beneficiaries}
               </p>
-              <p className="ml-auto text-[#1C065A] font-[500]">{sch_id}</p>
+              <p className="ml-auto text-[#1C065A] text-[14px] font-[500]">
+                {sch_id}
+              </p>
             </div>
             <Toaster />
 
@@ -161,10 +164,98 @@ const Schedule_payments = () => {
               </div>
             </div>
           </div>
+          {/* xs and sm */}
+          <div className="block md:hidden col-span-2 bg-white mx-3 md:mx-7">
+            <div className="mx-3  pt-6">
+              <h5 className="text-[#1C065A] font-[500] text-[14px]">{grade}</h5>
+              <p className="text-[14px] font-light text-[#4A5568]">
+                {no_of_beneficiaries}
+              </p>
+              <div className="flex items-center " onClick={copyToClipboard}>
+                <h5 className="font-light text-[14px] mr-2  text-[#4A5568]">
+                  Schedule Id
+                </h5>
+                <svg
+                  className="mr-9 cursor-pointer"
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g id="IoCopyOutline">
+                    <path
+                      id="Vector (Stroke)"
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M4.33594 3.375C3.80523 3.375 3.375 3.80523 3.375 4.33594V9.53906C3.375 10.0698 3.80523 10.5 4.33594 10.5H9.53906C10.0698 10.5 10.5 10.0698 10.5 9.53906V4.33594C10.5 3.80523 10.0698 3.375 9.53906 3.375H4.33594ZM2.625 4.33594C2.625 3.39101 3.39101 2.625 4.33594 2.625H9.53906C10.484 2.625 11.25 3.39101 11.25 4.33594V9.53906C11.25 10.484 10.484 11.25 9.53906 11.25H4.33594C3.39101 11.25 2.625 10.484 2.625 9.53906V4.33594Z"
+                      fill="black"
+                    />
+                    <path
+                      id="Vector (Stroke)_2"
+                      fill-rule="evenodd"
+                      clip-rule="evenodd"
+                      d="M2.62389 0.750003L2.625 0.75L7.68857 0.750002C8.13545 0.751275 8.56366 0.92936 8.87965 1.24535C9.19564 1.56134 9.37373 1.98956 9.375 2.43643L9.37492 2.44531L9.3632 3.00781C9.35889 3.21487 9.18753 3.37923 8.98047 3.37492C8.77341 3.37061 8.60905 3.19925 8.61336 2.99219L8.62498 2.43451C8.62321 2.18727 8.52422 1.95059 8.34932 1.77568C8.17357 1.59993 7.93545 1.50084 7.68692 1.5H2.62559C2.32739 1.50102 2.04168 1.61993 1.8308 1.8308C1.61993 2.04168 1.50102 2.32739 1.5 2.6256V7.68692C1.50084 7.93545 1.59993 8.17357 1.77568 8.34932C1.95143 8.52507 2.18957 8.62417 2.4381 8.625H3C3.20711 8.625 3.375 8.7929 3.375 9C3.375 9.20711 3.20711 9.375 3 9.375H2.4375L2.43643 9.375C1.98955 9.37373 1.56134 9.19564 1.24535 8.87965C0.929358 8.56366 0.751273 8.13545 0.750002 7.68857L0.75 7.6875V2.62389C0.751468 2.12736 0.949368 1.65158 1.30047 1.30047C1.65158 0.949369 2.12736 0.751471 2.62389 0.750003Z"
+                      fill="black"
+                    />
+                  </g>
+                </svg>
+              </div>
+            </div>
+            {/* second line */}
+            <div className="flex mx-3 ">
+              <p className=" text-[#1C065A] text-[14px] font-[500]">{sch_id}</p>
+            </div>
+            <Toaster />
+
+            <div
+              className="mx-3 h-[2px] border-b my-7 "
+              style={{ background: "var(--Color-Gray-Gray-40, #CBD5E0)" }}
+            ></div>
+            <div className="mx-3  ">
+              <p className="text-[#1C065A] text-[10px] font-[500]">
+                SCHEDULE DETAILS
+              </p>
+              <div className="flex mt-3">
+                <p className="text-[#718096] text-[14px] font-light">
+                  Schedule ID
+                </p>
+                <p className="text-[#718096] text-[14px] font-light ml-auto mr-[4.8rem]">
+                  Created On
+                </p>
+              </div>
+              <div className="flex">
+                <p className="text-[#1C065A] text-[14px] font-[500]">
+                  {daypayable}
+                </p>
+                <p className="text-[#1C065A] text-[14px] font-[500] ml-auto ">
+                  {createdon}
+                </p>
+              </div>
+              <div className="mt-3 ">
+                <div className="flex">
+                  <p className="text-[#718096] text-[14px] font-light">
+                    Time payable
+                  </p>
+                  <p className="text-[#718096] text-[14px] font-light ml-auto mr-[3.9rem]">
+                    Last Payment
+                  </p>
+                </div>
+                <div className="flex">
+                  <p className="text-[#1C065A] text-[14px] font-[500]">
+                    {timepayable}
+                  </p>
+                  <p className="text-[#1C065A] text-[14px] font-[500] ml-auto ">
+                    {lastpayment}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
 
           {/* second column */}
           <div className="cols-span-1">
-            <div className="flex mx-5 items-center">
+            <div className="flex mx-3 md:mx-5 mt-5 md:mt-0 items-center">
               <h1 className="text-[#1C065A] text-[16px] font-[600]">
                 Recent Payment
               </h1>
@@ -193,13 +284,13 @@ const Schedule_payments = () => {
                 </g>
               </svg>
             </div>
-            <div className="flex mx-5 bg-[#EBE3FC] px-[16px] py-[12px] rounded mt-4">
+            <div className="flex mx-3 md:mx-5  bg-[#EBE3FC] px-[16px] py-[12px] rounded mt-4">
               <p className="text-[#718096] text-[14px]">Total Paid:</p>
               <p className="ml-auto text-[#1C065A] text-[14px] font-[600]">
                 {total_paid}
               </p>
             </div>
-            <div className="grid-cols-2 grid-rows-6 mx-5">
+            <div className="grid-cols-2 grid-rows-6 mx-3 md:mx-5 ">
               <div className="flex mt-5">
                 <p className="text-[#1C065A] text-[14px] font-[600] ">
                   {monthly_salary}
@@ -262,7 +353,7 @@ const Schedule_payments = () => {
 
         <button
           onClick={openDeleteModal}
-          className=" my-9 text-[#FF5655] h-[32px] text-[14px] rounded w-[147px] mx-7 "
+          className=" my-9 text-[#FF5655] h-[32px] text-[14px] rounded w-[147px] mx-3 md:mx-7  "
           style={{
             border: "1px solid var(--Color-Base-Background-Color-4, #FF5655)",
           }}
