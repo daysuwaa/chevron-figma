dda1396a73974a2eebc1adbdbab5b64d87d12806
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-02-08 20:28:49 +0100

    edited the user component by giving the forms paddings and reduced the width of the modals

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,28 +1,9 @@
 import React, { useState } from "react";
 import Navbar from "../Stuff/Navbar";
-import man from "src/assets/image 1.png";
-import arrowdown from "src/assets/FiChevronDown.svg";
 import User from "./User";
 import Modal from "react-modal";
-import close from "src/assets/IoCloseOutline.svg";
-// import Tooltip from "@mui/material/Tooltip";
 
 const Users = () => {
-  const customStyles = {
-    content: {
-      top: "50%",
-      left: "50%",
-      right: "auto",
-      bottom: "auto",
-      marginRight: "-50%",
-      transform: "translate(-50%, -50%)",
-      padding: "2rem",
-      // background: "black",
-      // opacity: "0.2",
-      // backgroundColor: "#000",
-      // opacity: "0.5",
-    },
-  };
   const CustomModal = ({ isOpen, handleClose }) => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
@@ -36,118 +17,118 @@ const Users = () => {
       <Modal
         isOpen={isOpen}
         onRequestClose={handleClose}
-        style={customStyles}
-        contentLabel="Example Modal"
+        shouldCloseOnOverlayClick={false}
+        className="absolute top-1/2 left-[43%] md:left-1/2 w-[400px] md:w-[640px] transform -translate-x-1/2 bg-white -translate-y-1/2 p-[2rem] m-[2rem]"
+        overlayClassName="fixed inset-0 bg-black bg-opacity-50"
       >
-        <div className=" ">
-          <div className="flex justify-between items-center lg:w-[35rem] md:w-[30rem] w-full">
-            <h2 className="text-md  text-[#4A5568] mx-auto mb-4 font-semibold">
-              Add New User
-            </h2>
+        <div className="flex justify-between items-center ">
+          <h2 className="text-md  text-[#4A5568] mx-auto mb-4 font-semibold">
+            Add New User
+          </h2>
 
-            <button onClick={handleClose} className="text-gray-600">
-              <img src={close} alt="close" />
-            </button>
-          </div>
-          {/* lineee */}
-          <div
-            className="w-full h-[2px] border-b "
-            style={{ background: "var(--Color-Gray-Gray-40, #CBD5E0)" }}
-          ></div>
-
-          {/* the formsss */}
-          {/* #full name */}
-
-          <div className="my-6">
-            <form>
-              <label html="fname" className="text-[#4A5568] text-sm ">
-                Full Name
-              </label>
-              <div className="px-[10px] items-center h-[40px] flex w-[566px] rounded border border-[#CBD5E0] focus-within:outline outline-2 outline-blue-600">
-                <input
-                  type="text"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  placeholder="Enter Full Name"
-                  id="fname"
-                  className="font-normal w-[320px] h-8 outline-none"
-                  style={{
-                    color: "#718096",
-                    fontSize: "16px",
-                  }}
-                />
-              </div>
-            </form>
-          </div>
-          {/* #2 email adress */}
-          <div className="my-6">
-            <form>
-              <label htmlFor="emailll" className="text-[#4A5568] text-sm ">
-                Email Address
-              </label>
-              <div className="px-[10px] items-center h-[40px] flex w-[566px] rounded border border-[#CBD5E0] focus-within:outline outline-2 outline-blue-600">
-                <input
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder="Enter Email Address"
-                  id="emailll"
-                  className="font-normal w-[320px] h-8 outline-none text-lg"
-                  style={{
-                    color: "#718096",
-                    fontSize: "16px",
-                  }}
-                />
-              </div>
-            </form>
-          </div>
-          {/* #3 password */}
-          <div className="my-6">
-            <form>
-              <label htmlFor="password" className="text-[#4A5568] text-sm ">
-                Password
-              </label>
-              <div className="px-[10px] items-center h-[40px] flex w-[566px] rounded border border-[#CBD5E0] focus-within:outline outline-2 outline-blue-600">
-                <input
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  placeholder="Enter Password"
-                  id="passwordd"
-                  className="font-normal w-[320px] h-8 outline-none"
-                  style={{
-                    color: "#718096",
-                    fontSize: "16px",
-                  }}
-                />
-              </div>
-            </form>
-          </div>
-          {/* role */}
-          <div className="my-6">
-            <form>
-              <label className="flex text-[#4A5568] text-sm mb-2">Role</label>
-
-              <div className="px-[0px] items-center h-[40px] flex w-[566px] rounded border border-[#CBD5E0] focus-within:outline outline-2 outline-blue-600">
-                <select
-                  value={role}
-                  className="outline-none w-full "
-                  onChange={handleChange}
-                  placeholder="Selext Role"
-                >
-                  <option value="admin">Administration</option>
-                  <option value="Volvo">Accountant</option>
-                  <option value="Fiat">Auditor</option>
-                </select>
-              </div>
-            </form>
-            <button
-              className="bg-[#3B2774] h-[40px] w-[123px] rounded text-center text-white text-[16px] mt-9"
-              onClick={{}}
+          <button onClick={handleClose} className="text-gray-600">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              Create User
-            </button>
-          </div>
+              <g id="IoCloseOutline">
+                <path
+                  id="Vector (Stroke)"
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M6.21967 6.21967C6.51256 5.92678 6.98744 5.92678 7.28033 6.21967L12 10.9393L16.7197 6.21967C17.0126 5.92678 17.4874 5.92678 17.7803 6.21967C18.0732 6.51256 18.0732 6.98744 17.7803 7.28033L13.0607 12L17.7803 16.7197C18.0732 17.0126 18.0732 17.4874 17.7803 17.7803C17.4874 18.0732 17.0126 18.0732 16.7197 17.7803L12 13.0607L7.28033 17.7803C6.98744 18.0732 6.51256 18.0732 6.21967 17.7803C5.92678 17.4874 5.92678 17.0126 6.21967 16.7197L10.9393 12L6.21967 7.28033C5.92678 6.98744 5.92678 6.51256 6.21967 6.21967Z"
+                  fill="#4A5568"
+                />
+              </g>
+            </svg>
+          </button>
+        </div>
+        {/* lineee */}
+        <div className="w-full h-[2px] border-b bg-[#CBD5E0] "></div>
+
+        {/* the formsss */}
+        {/* #full name */}
+
+        <div className="my-6">
+          <form>
+            <label html="fname" className="text-[#4A5568] text-sm ">
+              Full Name
+            </label>
+            <div className="px-[10px] items-center h-[40px] mt-2 flex rounded border border-[#CBD5E0] focus-within:outline outline-2 outline-blue-600">
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Enter Full Name"
+                id="fname"
+                className="font-normal w-[320px]  outline-none text-[#718096] text-[16px]"
+              />
+            </div>
+          </form>
+        </div>
+        {/* #2 email adress */}
+        <div className="my-6">
+          <form>
+            <label htmlFor="emailll" className="text-[#4A5568] text-sm ">
+              Email Address
+            </label>
+            <div className="px-[10px] items-center h-[40px] mt-2 flex  rounded border border-[#CBD5E0] focus-within:outline outline-2 outline-blue-600">
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Enter Email Address"
+                id="emailll"
+                className="font-normal w-[320px] text-[#718096] text-[16px] outline-none text-lg"
+              />
+            </div>
+          </form>
+        </div>
+        {/* #3 password */}
+        <div className="my-6">
+          <form>
+            <label htmlFor="password" className="text-[#4A5568] text-sm ">
+              Password
+            </label>
+            <div className="px-[10px] items-center h-[40px] mt-2 flex  rounded border border-[#CBD5E0] focus-within:outline outline-2 outline-blue-600">
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Enter Password"
+                id="passwordd"
+                className="font-normal w-[320px]  outline-none text-[#718096] text-[16px]"
+              />
+            </div>
+          </form>
+        </div>
+        {/* role */}
+        <div className="my-6">
+          <form>
+            <label className="flex text-[#4A5568] text-sm">Role</label>
+
+            <div className="px-[0px] items-center h-[40px] flex mt-2  rounded border border-[#CBD5E0] focus-within:outline outline-2 outline-blue-600">
+              <select
+                value={role}
+                className="outline-none w-full "
+                onChange={handleChange}
+                placeholder="Selext Role"
+              >
+                <option value="admin">Administration</option>
+                <option value="Volvo">Accountant</option>
+                <option value="Fiat">Auditor</option>
+              </select>
+            </div>
+          </form>
+          <button
+            className="bg-[#3B2774] h-[40px] w-[123px] rounded text-center text-white text-[16px] mt-9"
+            onClick={{}}
+          >
+            Create User
+          </button>
         </div>
       </Modal>
     );
