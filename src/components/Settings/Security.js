88b34a147f8afe5645eac2ca030fa1621b1eb6ff
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-02-13 04:28:29 +0100

    fixed console errors and gave the form fields some paddings

--- src/components/Settings/Security.js
+++ src/components/Settings/Security.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import info from "src/assets/CkInfoOutline.svg";
 import Scan from "src/assets/image 4.png";
 import Tooltip from "@mui/material/Tooltip";
 import ToggleButton from "../Layout/ToggleButton";
@@ -9,10 +8,6 @@ const Security = () => {
   const [cpassword, setCpassword] = useState("");
   const [npassword, setNpassword] = useState("");
   const [confirm, setConfirm] = useState("");
-
-  // toggle button function
-
-  // checkbox function
 
   const Checkbox = () => {
     const [isChecked, setIsChecked] = useState(false);
@@ -24,6 +19,7 @@ const Security = () => {
     return (
       <label className="flex items-center mt-2 space-x-2">
         <input
+          id="check"
           type="checkbox"
           checked={isChecked}
           onChange={handleCheckboxChange}
@@ -60,62 +56,66 @@ const Security = () => {
         {/* formsssss */}
         <div className="m-6">
           <form>
-            <label htmlFor="ccpassword" className="text-[#4A5568] text-sm">
+            <label
+              htmlFor="ccpassword"
+              className="text-[#4A5568] text-sm pb-2 block"
+            >
               Current Password
             </label>
-            <div className="px-[10px] items-center h-[40px] flex lg:w-[566px] rounded border border-[#CBD5E0] focus-within:outline outline-2 outline-blue-600">
-              <input
-                type="password"
-                placeholder="Enter existing password"
-                value={cpassword}
-                onChange={(e) => setCpassword(e.target.value)}
-                id="ccpassword"
-                className="font-normal w-[320px] h-8 outline-none text-[#4A5568] text-[14px]"
-              />
-            </div>
+
+            <input
+              type="password"
+              placeholder="Enter existing password"
+              value={cpassword}
+              onChange={(e) => setCpassword(e.target.value)}
+              id="ccpassword" // Add id attribute
+              autoComplete="current-password"
+              className="font-normal px-4 text-[#718096] text-[14px] items-center h-[40px] flex w-full rounded border border-[#CBD5E0] focus-within:outline outline-2 outline-blue-600"
+            />
           </form>
         </div>
         <div className="m-6">
           <form>
-            <label htmlFor="npassword" className="text-[#4A5568] text-sm ">
+            <label
+              htmlFor="npassword"
+              className="text-[#4A5568] text-sm pb-2 block"
+            >
               New password
             </label>
-            <div className="px-[10px] items-center h-[40px] flex lg:w-[566px] rounded border border-[#CBD5E0] focus-within:outline outline-2 outline-blue-600">
-              <input
-                type="password"
-                placeholder="Enter new password"
-                value={npassword}
-                onChange={(e) => setNpassword(e.target.value)}
-                id="npassword"
-                className="font-normal w-[320px] h-8 outline-none text-[#4A5568] text-[14px] "
-              />
-            </div>
+
+            <input
+              type="password"
+              placeholder="Enter new password"
+              value={npassword}
+              onChange={(e) => setNpassword(e.target.value)}
+              id="npassword" // Add id attribute
+              autoComplete="new-password"
+              className="font-normal px-4 text-[#718096] text-[14px] items-center h-[40px] flex w-full rounded border border-[#CBD5E0] focus-within:outline outline-2 outline-blue-600"
+            />
           </form>
         </div>
         <div className="m-6">
           <form>
-            <label htmlFor="c-password" className="text-[#4A5568] text-sm ">
+            <label
+              htmlFor="confirmpassword"
+              className="text-[#4A5568] text-sm pb-2 block"
+            >
               Confirm Password
             </label>
-            <div className="px-[10px] items-center h-[40px] flex lg:w-[566px] rounded border border-[#CBD5E0] focus-within:outline outline-2 outline-blue-600">
-              <input
-                type="password"
-                placeholder="Re-type password"
-                id="c-password"
-                value={confirm}
-                onChange={(e) => setConfirm(e.target.value)}
-                className="font-normal w-[320px] h-8 outline-none text-[#4A5568] text-[14px]"
-              />
-            </div>
+
+            <input
+              type="password"
+              placeholder="Re-type password"
+              id="confirmpassword"
+              value={confirm}
+              onChange={(e) => setConfirm(e.target.value)}
+              autoComplete="new-password"
+              className="font-normal px-4 text-[#718096] text-[14px] items-center h-[40px] flex w-full rounded border border-[#CBD5E0] focus-within:outline outline-2 outline-blue-600"
+            />
           </form>
           {/* button */}
 
-          <button
-            className="text-[#1C065A]  items-center text-sm h-[32px] mt-7 rounded bg-white   w-[144px] text-center"
-            style={{
-              border: " 1px solid var(--Button-Background-Alternate, #3B2773)",
-            }}
-          >
+          <button className="text-[#1C065A]  items-center text-sm h-[32px] border border-[#3B2773] mt-7 rounded bg-white px-3   text-center">
             Update Password
           </button>
         </div>
@@ -130,7 +130,29 @@ const Security = () => {
           title="Fund your wallaet by paying into Guarantee Trust Account"
           placement="bottom"
         >
-          <img src={info} alt="info" className=" lg:ml-3 lg:mr-[19rem] px-3 " />
+          <svg
+            className="mx-2"
+            width="12"
+            height="12"
+            viewBox="0 0 12 12"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="CkInfoOutline" clipPath="url(#clip0_1050_818)">
+              <path
+                id="Vector"
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M1 6C1 3.23858 3.23858 1 6 1C8.76142 1 11 3.23858 11 6C11 8.76142 8.76142 11 6 11C3.23858 11 1 8.76142 1 6ZM6 0C2.68629 0 0 2.68629 0 6C0 9.31371 2.68629 12 6 12C9.31371 12 12 9.31371 12 6C12 2.68629 9.31371 0 6 0ZM5.97963 4.00007C6.25577 4.00007 6.47963 3.77621 6.47963 3.50007C6.47963 3.22393 6.25577 3.00007 5.97963 3.00007C5.70349 3.00007 5.47963 3.22393 5.47963 3.50007C5.47963 3.77621 5.70349 4.00007 5.97963 4.00007ZM6.47963 5.00014V5.50014V8.50014V9.00014H5.47963V8.50014V5.50014V5.00014H6.47963Z"
+                fill="#4A5568"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_1050_818">
+                <rect width="12" height="12" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
         </Tooltip>
         <ToggleButton className="" />
       </div>
@@ -144,7 +166,7 @@ const Security = () => {
         }}
       >
         <div className="lg:grid lg:grid-cols-2 m-6 ">
-          <div className="mx-auto">
+          <div>
             <p className="text-[#4A5568] font-normal text-lg mb-4">
               Enable 2FA
             </p>
@@ -169,49 +191,29 @@ const Security = () => {
               <form>
                 <input
                   type="numbers"
-                  id="number"
-                  className="font-normal mr-1 w-[30px] h-[30px] md:w-[40px] md:h-[40px] text-center outline-none"
-                  style={{
-                    color: "#718096",
-                    fontSize: "14px",
-                    border: "1px solid var(--gray-200, #E2E8F0)",
-                  }}
+                  id="num"
+                  className="font-normal text-[#718096] text-[14px] border border-[#E2E8F0] mr-1 w-[30px] h-[30px] md:w-[40px] md:h-[40px] text-center outline-none"
                 />
               </form>
               <form>
                 <input
                   type="number"
                   id="number2"
-                  className="font-normal mx-1  w-[30px] h-[30px] md:w-[40px] md:h-[40px] text-center outline-none"
-                  style={{
-                    color: "#718096",
-                    fontSize: "14px",
-                    border: "1px solid var(--gray-200, #E2E8F0)",
-                  }}
+                  className="font-normal text-[#718096] text-[14px] border border-[#E2E8F0] mr-1 w-[30px] h-[30px] md:w-[40px] md:h-[40px] text-center outline-none"
                 />
               </form>
               <form>
                 <input
                   type="number"
                   id="number3"
-                  className="font-normal w-[30px] h-[30px] md:w-[40px] md:h-[40px] mx-1  text-center outline-none"
-                  style={{
-                    color: "#718096",
-                    fontSize: "14px",
-                    border: "1px solid var(--gray-200, #E2E8F0)",
-                  }}
+                  className="font-normal text-[#718096] text-[14px] border border-[#E2E8F0] mr-1 w-[30px] h-[30px] md:w-[40px] md:h-[40px] text-center outline-none"
                 />
               </form>
               <form>
                 <input
                   type="number"
                   id="number4"
-                  className="font-normal w-[30px] h-[30px] md:w-[40px] md:h-[40px] mx-1  text-center outline-none"
-                  style={{
-                    color: "#718096",
-                    fontSize: "14px",
-                    border: "1px solid var(--gray-200, #E2E8F0)",
-                  }}
+                  className="font-normal text-[#718096] text-[14px] border border-[#E2E8F0] mr-1 w-[30px] h-[30px] md:w-[40px] md:h-[40px] text-center outline-none"
                 />
               </form>
 
@@ -219,25 +221,15 @@ const Security = () => {
                 <input
                   type="number"
                   id="number5"
-                  className="font-normal w-[30px] h-[30px] md:w-[40px] md:h-[40px] mx-1  text-center outline-none"
-                  style={{
-                    color: "#718096",
-                    fontSize: "14px",
-                    border: "1px solid var(--gray-200, #E2E8F0)",
-                  }}
+                  className="font-normal text-[#718096] text-[14px] border border-[#E2E8F0] mr-1 w-[30px] h-[30px] md:w-[40px] md:h-[40px] text-center outline-none"
                 />
               </form>
 
               <form>
                 <input
                   type="text"
-                  id="search"
-                  className="font-normal w-[30px] h-[30px] md:w-[40px] md:h-[40px] mx-1  text-center outline-none"
-                  style={{
-                    color: "#718096",
-                    fontSize: "14px",
-                    border: "1px solid var(--gray-200, #E2E8F0)",
-                  }}
+                  id="number6"
+                  className="font-normal text-[#718096] text-[14px] border border-[#E2E8F0] mr-1 w-[30px] h-[30px] md:w-[40px] md:h-[40px] text-center outline-none"
                 />
               </form>
             </div>
@@ -245,13 +237,7 @@ const Security = () => {
 
             {/* button */}
 
-            <button
-              className="text-[#1C065A] text-center  text-sm h-[32px] mt-7 rounded bg-white ] items-center w-[100px] "
-              style={{
-                border:
-                  " 1px solid var(--Button-Background-Alternate, #3B2773)",
-              }}
-            >
+            <button className="text-[#1C065A] text-center border border-[#3B2773]  text-sm h-[32px] mt-7 rounded bg-white ] items-center w-[100px] ">
               Enable 2FA
             </button>
           </div>
