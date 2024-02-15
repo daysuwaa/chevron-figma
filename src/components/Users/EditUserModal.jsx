c7084582cb6bb259378908ee6dca6ba5432a466a
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2024-02-15 09:49:17 +0100

    edited the width of the edit modal

--- src/components/Users/EditUserModal.jsx
+++ src/components/Users/EditUserModal.jsx
@@ -10,8 +10,8 @@ const EditUserModal = ({ isEditModalOpen, closeEditModal }) => {
   };
   return (
     isEditModalOpen && (
-      <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
-        <div className="bg-white p-6 rounded lg:w-[33rem] md:w-[30rem]">
+      <div className="fixed top-0 ring left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
+        <div className="bg-white p-6 ring rounded w-[20rem] lg:w-[43rem] md:w-[30rem]">
           <div className="flex justify-center">
             <p className="mx-auto text-md font-semibold  text-[#4A5568]">
               Edit User
